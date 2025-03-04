7fcddfe84431b6288dc862ee82ad8bffc5fa32ac
Ubuntu <azureuser@mtc-vm.rzgxkum4defuliyknfhvxx5b0d.ax.internal.cloudapp.net>
2025-03-04 13:36:35 +0000

    added py file

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route('/')
+def home():
+    return "Hello, Mr Kingsley Ibomo!"
+
+# Ensure the app runs on 0.0.0.0 to be accessible outside the container
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
