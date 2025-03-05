b28d71198d07b246a47c2181130922b373fae6c3
Ubuntu <azureuser@mtc-vm.rzgxkum4defuliyknfhvxx5b0d.ax.internal.cloudapp.net>
2025-03-05 22:42:07 +0000

    edited python file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the homepage
 @app.route('/')
 def home():
-    return "Hello guys, this is my Network Administrations project!"
+    return "Hello Mr Kingsley Ibomo"
 
 # Ensure the app runs on 0.0.0.0 to be accessible outside the container
 if __name__ == "__main__":
