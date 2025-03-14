6d4be0a9e3f13bb3194716d49f834192f43f55de
vanen234 <ibivanen0@gmail.com>
2025-03-14 08:15:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def home():
     </head>
     <body>
         <div class="container">
-            <h1> Welcome to our Network Administrations app Mr Kingsley Ibomo! </h1>
+            <h1> Live Demo </h1>
             <p>Hope you like it, and welcome fellow classmates! 😎</p>
         </div>
     </body>
